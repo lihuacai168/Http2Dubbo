9334bc9c44f84c488ec59c3c144ef955b58d9f1c
lihuacai <lihuacai168@gmail.com>
2020-10-04 15:23:48 +0800

    1.修改：配置文件改成dev

--- Http2Dubbo/settings/base.py
+++ Http2Dubbo/settings/base.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '83$vv2v91w453ipq&&anm+q!ihffz3r4f_^&7t0ddmgehpr6!r'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,17 +78,15 @@ REST_FRAMEWORK = {
 
 WSGI_APPLICATION = 'Http2Dubbo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -124,10 +119,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-DUBBO_CONFIG = [{'host': '192.168.22.2', 'port': 20070}, {'host': '192.168.22.3', 'port': 20070},
-                {'host': '192.168.22.4', 'port': 20072}]
+
---
+++ Http2Dubbo/settings/dev.py
@@ -0,0 +1,3 @@
+from .base import *
+DEBUG = True
+DUBBO_CONFIG = [{'host': 'localhost', 'port': 20880}]
